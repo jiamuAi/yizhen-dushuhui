157e2227e4679f8ca8000317dfeac5eee9765d76
jiamu <jiamuAi@gmail.com>
2025-05-14 18:33:19 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: './', // 👈 添加这一行，让静态资源使用相对路径
+})
